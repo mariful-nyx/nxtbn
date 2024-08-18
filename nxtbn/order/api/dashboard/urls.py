6b8901b74e3e4bab26157cb45c6c7125adfebaa4
MySecondLanguage <bytenyxlimited@gmail.com>
2024-08-19 00:46:22 +0600

    order state api added

--- nxtbn/order/api/dashboard/urls.py
+++ nxtbn/order/api/dashboard/urls.py
@@ -6,5 +6,5 @@ from nxtbn.order.api.dashboard import views as order_views
 urlpatterns = [
     path('orders/', order_views.OrderListView.as_view(), name='order-list'),
     path('orders/<uuid:id>/', order_views.OrderDetailView.as_view(), name='order-detail'),
-    path('stats/', order_views.OrderStatsView.as_view(), name='order-stats'),
+    path('stats/', order_views.BasicStatsView.as_view(), name='basic-stats'),
 ]

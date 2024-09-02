eb94ec3b138475ed9a049bdce05a4d5c853bb006
MySecondLanguage <bytenyxlimited@gmail.com>
2024-09-02 16:17:39 +0600

    User rendered and customer api added

--- nxtbn/users/api/dashboard/serializers.py
+++ nxtbn/users/api/dashboard/serializers.py
@@ -2,7 +2,22 @@ from django.utils.translation import gettext_lazy as _
 from rest_framework import serializers
 from django.db import transaction
 
+from nxtbn.users.admin import User
+
 
 class DashboardLoginSerializer(serializers.Serializer):
     email = serializers.EmailField(required=True)
     password = serializers.CharField(write_only=True, required=True)
+
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'avatar', 'username', 'email', 'first_name', 'last_name', 'role']
+
+
+class CustomerSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'avatar', 'username', 'email', 'first_name', 'last_name', 'full_name', 'role']
--- nxtbn/users/models.py
+++ nxtbn/users/models.py
@@ -8,3 +8,6 @@ from nxtbn.users import UserRole
 class User(AbstractUser):
     role = models.CharField(max_length=255, choices=UserRole.choices, default=UserRole.CUSTOMER)
     avatar = models.ImageField(upload_to='avatars/', null=True, blank=True)
+
+    def full_name(self):
+        return f"{self.first_name} {self.last_name}"

cc3adaa24456868f3c648095ffdd13ad4a25b953
mariful-nyx <mariful@heliosinsider.com>
2024-09-05 11:08:21 +0600

    change password of user admin

--- nxtbn/users/api/dashboard/serializers.py
+++ nxtbn/users/api/dashboard/serializers.py
@@ -1,6 +1,7 @@
 from django.utils.translation import gettext_lazy as _
 from rest_framework import serializers
 from django.db import transaction
+from django.contrib.auth.password_validation import validate_password
 
 from nxtbn.users.admin import User
 
@@ -8,7 +9,6 @@ from nxtbn.users.admin import User
 class DashboardLoginSerializer(serializers.Serializer):
     email = serializers.EmailField(required=True)
     password = serializers.CharField(write_only=True, required=True)
-
 
 
 class UserSerializer(serializers.ModelSerializer):
@@ -21,3 +21,30 @@ class CustomerSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ['id', 'avatar', 'username', 'email', 'first_name', 'last_name', 'full_name', 'role']
+
+
+class PasswordChangeSerializer(serializers.Serializer):
+    old_password = serializers.CharField(required=True)
+    new_password = serializers.CharField(required=True, 
+                                         #validators=[validate_password]
+                                        )
+
+    def validate_old_password(self, value):
+        user = self.context["request"].user
+        if not user.check_password(value):
+            raise serializers.ValidationError("Old password is not correct")
+        return value
+
+    def validate(self, attrs):
+        if attrs["old_password"] == attrs["new_password"]:
+            raise serializers.ValidationError(
+                "New password cannot be the same as the old password"
+            )
+        return attrs
+
+    def save(self, **kwargs):
+        user = self.context["request"].user
+        user.set_password(self.validated_data["new_password"])
+        user.save()
+        return user
+

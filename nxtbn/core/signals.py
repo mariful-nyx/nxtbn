f4dfa0c77eb3fec915508c274f322502c14a8db8
MySecondLanguage <bytenyxlimited@gmail.com>
2024-09-08 09:09:33 +0600

    Implemented Order invoice API

--- nxtbn/core/signals.py
+++ nxtbn/core/signals.py
@@ -1,8 +1,22 @@
 from django.db.models.signals import post_migrate
 from django.dispatch import receiver
 from nxtbn.core.models import SiteSettings
+from django.contrib.sites.models import Site
+
 
 @receiver(post_migrate)
 def create_default_site_settings(sender, **kwargs):
     if SiteSettings.objects.count() == 0:
-        SiteSettings.objects.create(site_name="nxtbn commerce")
+        site, created = Site.objects.get_or_create(
+            domain="example.com", defaults={"name": "Default Site"}
+        )
+        
+        SiteSettings.objects.create(
+            site=site,
+            site_name="nxtbn commerce",
+            company_name="Default Company Name",
+            contact_email="contact@example.com",
+            contact_phone="123456789",
+            address="Default Address",
+        )
+        print("SiteSettings instance created.")
--- nxtbn/invoice/api/dashboard/views.py
+++ nxtbn/invoice/api/dashboard/views.py
@@ -3,3 +3,12 @@ from rest_framework.response import Response
 from django.utils.translation import gettext_lazy as _
 from rest_framework.permissions  import AllowAny
 from rest_framework.exceptions import APIException
+
+from nxtbn.invoice.api.dashboard.serializers import OrderInvoiceSerializer
+from nxtbn.order.models import Order
+
+
+class OrderInvoiceAPIView(generics.RetrieveAPIView):
+    queryset = Order.objects.all()
+    serializer_class = OrderInvoiceSerializer
+    lookup_field = 'alias'

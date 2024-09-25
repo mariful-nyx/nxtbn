1fd856f5c9a999bd3312ea1ca260b9a44aaffae0
MySecondLanguage <bytenyxlimited@gmail.com>
2024-09-25 15:20:32 +0600

    Registered tax class and tax rate in admin

--- nxtbn/tax/admin.py
+++ nxtbn/tax/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
-# Register your models here.
+from nxtbn.tax.models import TaxClass, TaxRate
+
+class TaxRateInline(admin.TabularInline):
+    model = TaxRate
+    extra = 1
+
+class TaxClassAdmin(admin.ModelAdmin):
+    inlines = [TaxRateInline]
+
+admin.site.register(TaxClass, TaxClassAdmin)

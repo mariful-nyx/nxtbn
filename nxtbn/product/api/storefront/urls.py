35ac1926a3411748cb178fd57019ee05a9ed8e9b
MySecondLanguage <bytenyxlimited@gmail.com>
2024-10-10 11:16:42 +0600

    storefront product api made viewsets

--- nxtbn/product/api/storefront/urls.py
+++ nxtbn/product/api/storefront/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
+from rest_framework.routers import DefaultRouter
+
 from nxtbn.product.api.storefront import views as product_views
+from django.urls import include
+
+router = DefaultRouter()
+router.register(r'products', product_views.ProductViewSet, basename='product')
 
 urlpatterns = [
-    path('products/', product_views.ProductListView.as_view(), name='product-list'),
+    path('', include(router.urls)),
     path('products/<slug:slug>/', product_views.ProductDetailView.as_view(), name='product-detail'),
     path('collections/', product_views.CollectionListView.as_view(), name='collection-list'),
     path('recursive-categories/', product_views.CategoryListView.as_view(), name='category-list'),

0f6e075d042d3618257f3bafc6410b33f4ecdbfa
MySecondLanguage <bytenyxlimited@gmail.com>
2024-10-18 21:05:20 +0600

    cart removed on order create

--- nxtbn/order/receivers.py
+++ nxtbn/order/receivers.py
@@ -1,10 +1,8 @@
 from django.dispatch import receiver
+from nxtbn.cart.utils import remove_ordered_items_from_cart
 from nxtbn.core.signal_initiators import order_created
 
 @receiver(order_created)
-def rectify_cart_on_order_create(sender, order, request, **kwargs):
-    """
-    Remove item that is in the order from the cart, unordered items will remain.
-    """
-    print('Order created signal received=======================================================.')
+def handle_post_order_create(sender, order, request, **kwargs):
+    remove_ordered_items_from_cart(order, request=request)
    

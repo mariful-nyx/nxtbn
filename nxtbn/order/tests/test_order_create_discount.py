c5d1316f3e9d9e5625783cd9834320acd31f2430
MySecondLanguage <mmamunism@gmail.com>
2024-12-02 15:39:00 +0600

    Discount

--- nxtbn/order/tests/test_order_create_discount.py
+++ nxtbn/order/tests/test_order_create_discount.py
@@ -5,6 +5,8 @@ from rest_framework import status
 from rest_framework.reverse import reverse
 from nxtbn.core import PublishableStatus
 from nxtbn.core.utils import build_currency_amount, normalize_amount_currencywise
+from nxtbn.discount import PromoCodeType
+from nxtbn.discount.tests import PromoCodeFactory
 from nxtbn.home.base_tests import BaseTestCase
 from nxtbn.order.proccesor.views import get_shipping_rate_instance
 from nxtbn.product import WeightUnits
@@ -13,7 +15,7 @@ from rest_framework.test import APIClient
 from nxtbn.product.tests import ProductFactory, ProductTypeFactory, ProductVariantFactory
 from nxtbn.shipping.models import ShippingRate
 from nxtbn.shipping.tests import ShippingMethodFactory, ShippingRateFactory
-from nxtbn.tax.tests import TaxClassFactory
+from nxtbn.tax.tests import TaxClassFactory, TaxRateFactory
 from babel.numbers import get_currency_precision, format_currency
 
 
@@ -26,17 +28,13 @@ class TestOrderCreateWithDiscountRate(BaseTestCase):
         self.country = 'US'
         self.state = 'NY'
         self.precision = str(get_currency_precision(settings.BASE_CURRENCY))
-
-        # Shipping method
-        self.shipping_method = ShippingMethodFactory(name='DHL-DTH')
-
         
 
         self.test_cases = {
             '2': { # USD, EUR, BDT etc.
                 'input_weight_min': 0,
                 'input_weight_max': 5,  # 5kg
-                'input_incremental_rate': 3,  # 3 USD
+                'order_discount_code': 'DISCOUNT10P',
 
                 'variant_one_price': 50.29,
                 'variant_one_wv': 100,  # 100 grams
@@ -44,12 +42,12 @@ class TestOrderCreateWithDiscountRate(BaseTestCase):
                 'variant_two_price': 20.26,
                 'variant_two_wv': 578,
                 'variant_two_oqty': 1,
-                'order_discount_code': 'DISCOUNT10',
+                
             },
             '3': { # KWD, OMR, etc.
                 'input_weight_min': 0,
                 'input_weight_max': 5,  # 5kg
-                'order_discount_code': 'DISCOUNT10',
+                'order_discount_code': 'DISCOUNT10P',
 
                 'variant_one_price': 50.290,
                 'variant_one_wv': 100,  # 100 grams
@@ -61,8 +59,8 @@ class TestOrderCreateWithDiscountRate(BaseTestCase):
             '0': { # JPY, KRW, etc.
                 'input_weight_min': 0,
                 'input_weight_max': 5,  # 5kg
-                'input_rate': 15,  # 15 USD
-                'input_incremental_rate': 3,  # 3 USD
+                'order_discount_code': 'DISCOUNT10P',
+
                 'variant_one_price': 50,
                 'variant_one_wv': 100,  # 100 grams
                 'variant_one_oqty': 40,
@@ -72,24 +70,21 @@ class TestOrderCreateWithDiscountRate(BaseTestCase):
             },
         }
 
-       
-        ShippingRateFactory(
-            shipping_method=self.shipping_method,
-            country=self.country,
-            region=self.state,
-            rate=normalize_amount_currencywise(self.test_cases[self.precision]['input_rate'], settings.BASE_CURRENCY),
-            weight_min=self.test_cases[self.precision]['input_weight_min'],
-            weight_max=self.test_cases[self.precision]['input_weight_max'],  
-            currency=settings.BASE_CURRENCY,
-            incremental_rate=normalize_amount_currencywise(self.test_cases[self.precision]['input_incremental_rate'], settings.BASE_CURRENCY)
-        )
-
-       
 
         self.order_api_url = reverse('order-create')
         self.order_estimate_api_url = reverse('order-estimate')
 
-    def test_order_shipping_rate_calculation(self):
+        self.discount = PromoCodeFactory(
+            code=self.test_cases[self.precision]['order_discount_code'],
+            code_type=PromoCodeType.PERCENTAGE,
+            value=10,
+            is_active=True,
+            min_purchase_amount=0,
+            min_purchase_period=None,
+            redemption_limit=None,
+        )
+
+    def test_order_discount_calculation(self):
         """
         Test case to ensure shipping rates are accurately calculated based on product weights and regions.
         """
@@ -99,9 +94,10 @@ class TestOrderCreateWithDiscountRate(BaseTestCase):
         product_type = ProductTypeFactory(
             name=f"{currency} Product Type physical",
             track_stock=False,
-            taxable=True,
+            taxable=False,
             physical_product=True,
         )
+
 
         # Create products and variants
         product_one = ProductFactory(
@@ -154,7 +150,7 @@ class TestOrderCreateWithDiscountRate(BaseTestCase):
                 "last_name": "Doe",
                 "phone_number": "1234567890"
             },
-            'shipping_method_id': self.shipping_method.id,
+            "promocode": self.test_cases[self.precision]['order_discount_code'],
             "variants": [
                 {
                     "alias": variant_one.alias,
@@ -167,43 +163,24 @@ class TestOrderCreateWithDiscountRate(BaseTestCase):
             ]
         }
 
-        # Expected values
-        total_weight = Decimal(
-            (self.test_cases[self.precision]['variant_one_oqty'] * self.test_cases[self.precision]['variant_one_wv'] + self.test_cases[self.precision]['variant_two_oqty'] * self.test_cases[self.precision]['variant_two_wv']) / 1000
-        )  # Expected 4.578
-
-       
-
-        address = {
-            'country': self.country,
-            'state': self.state,
-        }
-        shipping_rate_instance = get_shipping_rate_instance(
-            self.shipping_method.id,
-            address,
-            total_weight,
+        subtotal = Decimal(
+            self.test_cases[self.precision]['variant_one_price'] * self.test_cases[self.precision]['variant_one_oqty'] +
+            self.test_cases[self.precision]['variant_two_price'] * self.test_cases[self.precision]['variant_two_oqty']
         )
+        total_discount = subtotal * Decimal(self.discount.value) / 100
 
-        if shipping_rate_instance:
-            self.shipping_rate = shipping_rate_instance.rate
-            self.test_cases[self.precision]['input_incremental_rate'] = shipping_rate_instance.incremental_rate
        
-
-        shipping_cost = (
-            self.shipping_rate
-            if total_weight <= self.test_cases[self.precision]['input_weight_max']
-            else self.shipping_rate + (total_weight - Decimal(5)) *  self.test_cases[self.precision]['input_incremental_rate']
-        ) 
         expected_subtotal = Decimal(
             self.test_cases[self.precision]['variant_one_price'] * self.test_cases[self.precision]['variant_one_oqty'] +
             self.test_cases[self.precision]['variant_two_price'] * self.test_cases[self.precision]['variant_two_oqty']
         )
-        expected_total = expected_subtotal + shipping_cost 
+        expected_total = expected_subtotal - total_discount
 
 
         expected_total_fr = build_currency_amount(expected_total, currency, locale='en_US')
         expected_subtotal_fr = build_currency_amount(expected_subtotal, currency, locale='en_US')
-        expected_shipping_cost_fr = build_currency_amount(shipping_cost, currency, locale='en_US')
+        expected_total_discount_fr = build_currency_amount(total_discount, currency, locale='en_US')
+        
 
         
 
@@ -213,11 +190,13 @@ class TestOrderCreateWithDiscountRate(BaseTestCase):
         self.assertEqual(order_estimate_response.status_code, status.HTTP_200_OK)
         self.assertEqual(order_estimate_response.data['subtotal'], expected_subtotal_fr)
         self.assertEqual(order_estimate_response.data['total'], expected_total_fr)
-        self.assertEqual(order_estimate_response.data['shipping_fee'], expected_shipping_cost_fr)
+        self.assertEqual(order_estimate_response.data['discount'], expected_total_discount_fr)
+       
 
         # Order Create Test
         order_response = self.client.post(self.order_api_url, order_payload, format='json')
         self.assertEqual(order_response.status_code, status.HTTP_200_OK)
         self.assertEqual(order_response.data['subtotal'], expected_subtotal_fr)
         self.assertEqual(order_response.data['total'], expected_total_fr)
-        self.assertEqual(order_response.data['shipping_fee'], expected_shipping_cost_fr)
+        self.assertEqual(order_response.data['discount'], expected_total_discount_fr)
+       

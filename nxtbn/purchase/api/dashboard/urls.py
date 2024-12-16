e6ccf150018823ec3fa49b818a5bb89d51f9cf23
MySecondLanguage <mmamunism@gmail.com>
2024-12-16 12:23:51 +0600

    Added purchase api views

--- nxtbn/purchase/api/dashboard/urls.py
+++ nxtbn/purchase/api/dashboard/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
 from nxtbn.purchase.api.dashboard import views
+from rest_framework.routers import DefaultRouter
 
-urlpatterns = [
-    path('purchase-orders/', views.PurchaseOrderList.as_view(), name='purchase-order-list')
+
+router = DefaultRouter()
+router.register(r'purchase-orders', views.PurchaseViewSet)
+
+urlpatterns = router.urls
+
+urlpatterns += [
+    
 ]
-
-
--- nxtbn/purchase/api/dashboard/views.py
+++ nxtbn/purchase/api/dashboard/views.py
@@ -1,10 +1,15 @@
-from rest_framework import generics
-from nxtbn.purchase.api.dashboard.serializers import PurchaseOrderSerializer
+from rest_framework import generics, viewsets
+from nxtbn.purchase.api.dashboard.serializers import PurchaseOrderCreateSerializer, PurchaseOrderSerializer
 from nxtbn.purchase.models import PurchaseOrder
 from nxtbn.core.paginator import NxtbnPagination
 
 
-class PurchaseOrderList(generics.ListAPIView):
+class PurchaseViewSet(viewsets.ModelViewSet):
     queryset = PurchaseOrder.objects.all()
     serializer_class = PurchaseOrderSerializer
     pagination_class = NxtbnPagination
+
+    def get_serializer_class(self):
+        if self.request.method == 'POST':
+            return PurchaseOrderCreateSerializer
+        return self.get_serializer_class

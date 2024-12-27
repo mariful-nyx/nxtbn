d3a148b6a7663dd65ad26884ea42643400cecd5f
MySecondLanguage <mmamunism@gmail.com>
2024-12-28 05:36:46 +0600

    Added docker igore

--- nxtbn/settings.py
+++ nxtbn/settings.py
@@ -251,8 +251,8 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_DIRS = [
-        os.path.join(BASE_DIR, 'static'),
-        os.path.join(BASE_DIR, 'admin-build/static'),
+    os.path.join(BASE_DIR, 'static'),
+    # os.path.join(BASE_DIR, 'admin-build/static'),  # No longer needed as cdn is used
 ]
 
 MEDIA_URL = '/media/'

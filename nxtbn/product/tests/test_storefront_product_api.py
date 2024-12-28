e317c6b4695534e629f4f522c1865f16e45f3716
MySecondLanguage <mmamunism@gmail.com>
2024-12-28 09:05:23 +0600

    tested product list

---
+++ nxtbn/product/tests/test_storefront_product_api.py
@@ -0,0 +1,44 @@
+# test case for single currency mode
+from decimal import Decimal
+import random
+import sys
+from django.conf import settings
+from rest_framework import status
+from rest_framework.reverse import reverse
+from nxtbn.core import PublishableStatus
+from nxtbn.core.utils import normalize_amount_currencywise
+from nxtbn.home.base_tests import BaseTestCase
+from nxtbn.product.models import Product, ProductType
+from rest_framework.test import APIClient
+from nxtbn.product.tests import ProductFactory, ProductTypeFactory, ProductVariantFactory
+from nxtbn.tax.tests import TaxClassFactory, TaxRateFactory
+
+
+from django.test.utils import override_settings
+
+
+@override_settings(CELERY_TASK_ALWAYS_EAGER=True)
+class ProductListWithDefaultVariantOnlyAPITest(BaseTestCase):
+
+    def setUp(self):
+        super().setUp()
+        self.adminLogin()
+
+        self.products = []
+        for _ in range(20):
+            product_type = ProductTypeFactory()
+            product = ProductFactory(product_type=product_type)
+            default_variant = ProductVariantFactory(product=product, price=Decimal('157.69'))
+            product.default_variant = default_variant
+            product.save()
+            self.products.append(product)
+
+        
+    def test_product_list_with_default_variant_only(self):
+        url = reverse('product-list')
+        response = self.auth_client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data['results']), 20)
+        
+        for product in response.data['results']:
+            self.assertNotIn('variants', product) # only default variant should be present, not all variants

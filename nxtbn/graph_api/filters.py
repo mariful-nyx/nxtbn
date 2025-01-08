7fe6393f49ca4aa33339665b4bc504e806d0b40c
MySecondLanguage <mmamunism@gmail.com>
2025-01-08 10:24:14 +0600

    Added search

--- nxtbn/graph_api/filters.py
+++ nxtbn/graph_api/filters.py
@@ -1,8 +1,10 @@
 import django_filters as filters
 from nxtbn.product.models import Category, Collection, Product, Supplier
+from django.db.models import Q
 
 
 class ProductFilter(filters.FilterSet):
+    search = filters.CharFilter(method='filter_search', label='Search Across Multiple Fields')
     name = filters.CharFilter(lookup_expr='icontains')
     summary = filters.CharFilter(lookup_expr='icontains')
     description = filters.CharFilter(lookup_expr='icontains')
@@ -10,10 +12,16 @@ class ProductFilter(filters.FilterSet):
     category_name = filters.CharFilter(field_name='category__name', lookup_expr='icontains')
     supplier = filters.ModelChoiceFilter(field_name='supplier', queryset=Supplier.objects.all())
     brand = filters.CharFilter(lookup_expr='icontains')
-    type = filters.CharFilter(field_name='type', lookup_expr='exact')
     related_to = filters.CharFilter(field_name='related_to__name', lookup_expr='icontains')
     collection = filters.ModelChoiceFilter(field_name='collections', queryset=Collection.objects.all())
 
     class Meta:
         model = Product
-        fields = ('name', 'summary', 'description', 'category', 'category_name', 'supplier', 'brand', 'type', 'related_to', 'collection')
+        fields = ('name', 'summary', 'description', 'category', 'category_name', 'supplier', 'brand','related_to', 'search', 'collection')
+
+    def filter_search(self, queryset, name, value):
+        """
+        Custom search method to search across multiple fields: name, description, and alias.
+        """
+        lookup = Q(name__icontains=value) | Q(description__icontains=value) | Q(alias__icontains=value)
+        return queryset.filter(lookup)

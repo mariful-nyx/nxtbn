2b4557432e54972a1242f4f5a90530a13c950992
MySecondLanguage <mmamunism@gmail.com>
2025-01-10 13:38:36 +0600

    Handled permission

--- nxtbn/core/admin_permissions.py
+++ nxtbn/core/admin_permissions.py
@@ -45,3 +45,16 @@ class RoleBasedPermission(BasePermission):
             return True
 
         return False
+    
+
+def check_user_permissions(info, allowed_roles):
+    if not info.context.user:
+        raise Exception("You must be logged in to perform this action")
+    
+    if not info.context.user.is_staff:
+        raise Exception("You must be a staff to perform this action")
+    
+    if info.context.user.role not in allowed_roles:
+        raise Exception("You do not have permission to perform this action")
+    
+    return True
--- nxtbn/product/admin_queries.py
+++ nxtbn/product/admin_queries.py
@@ -3,9 +3,11 @@
 import graphene
 
 from graphene_django.filter import DjangoFilterConnectionField
+from nxtbn.core.admin_permissions import check_user_permissions
 from nxtbn.product.admin_types import CategoryType, CollectionType, ProductGraphType, ProductTagType, ProductTranslationType, ProductVariantType, SupplierType
 from nxtbn.product.models import Product, ProductTranslation
 from nxtbn.product.storefront_types import CategoryHierarchicalType
+from nxtbn.users import UserRole
 
 
 class ProductTranslationQuery(graphene.ObjectType):
@@ -24,19 +26,24 @@ class ProductTranslationQuery(graphene.ObjectType):
     hierarchical_categories = DjangoFilterConnectionField(CategoryHierarchicalType)
 
     def resolve_product(root, info, id):
+        check_user_permissions(info, [UserRole.ADMIN, UserRole.STORE_MANAGER, UserRole.PRODUCT_MANAGER, UserRole.MARKETING_MANAGER])
+
         try:
             return Product.objects.get(pk=id)
         except Product.DoesNotExist:
             return None
         
     def resolve_all_products(root, info, **kwargs):
+        check_user_permissions(info, [UserRole.ADMIN, UserRole.STORE_MANAGER, UserRole.PRODUCT_MANAGER, UserRole.MARKETING_MANAGER])
         return Product.objects.all()
     
     def resolve_product_translation(root, info, base_product_id, lang_code):
+        check_user_permissions(info, [UserRole.ADMIN, UserRole.STORE_MANAGER, UserRole.PRODUCT_MANAGER, UserRole.MARKETING_MANAGER])
         try:
             return ProductTranslation.objects.get(product_id=base_product_id, language_code=lang_code)
         except ProductTranslation.DoesNotExist:
             return None
         
     def resolve_all_product_translations(root, info, **kwargs):
+        check_user_permissions(info, [UserRole.ADMIN, UserRole.STORE_MANAGER, UserRole.PRODUCT_MANAGER, UserRole.MARKETING_MANAGER])
         return ProductTranslation.objects.all()

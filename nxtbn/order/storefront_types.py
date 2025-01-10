f9fdf4b8cf7f9c021cebffa886fab2c8ae6386fe
MySecondLanguage <mmamunism@gmail.com>
2025-01-10 16:47:43 +0600

    Storfront qouery fixed

--- nxtbn/order/storefront_types.py
+++ nxtbn/order/storefront_types.py
@@ -1,21 +1,20 @@
+import graphene
 from graphene_django import DjangoObjectType
 from nxtbn.order.models import Address
 
 class AddressGraphType(DjangoObjectType):
+    db_id = graphene.Int(source='id')
     class Meta:
         model = Address
         fields = (
             'id',
             'first_name',
             'last_name',
-            'company_name',
             'street_address',
             'city',
             'postal_code',
             'country',
-            'phone',
+            'phone_number',
             'email',
             'address_type',
-            'created_at',
-            'updated_at',
         )
